e3ed09118a164f61208711b6d62fe199457ac8bd
Het <hetp111@users.noreply.github.com>
2021-04-02 00:00:08 +0530

    hbs to ejs, fix pending = promotion,multiple users

---
+++ app/aap.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+// set the view engine to use handlebars
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Chess Application',
+        page_title: 'Chess Board'
+    });
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('./app/aap');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const socketSever = require('./app/controllers/socketServer');
+socketSever(io);
+
+//set port and listen request
+const PORT = process.env.PORT || 8080;
+
+http.listen(PORT, () => {
+    console.log('current server runing on PORT : '+PORT);
+});
